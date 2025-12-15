63c9ef908d7db809e699c873a23458b742965359
Jade <jade@justbetter.nl>
2025-12-15 08:38:00 +0100

    v5 compatibility

--- src/MsiServiceProvider.php
+++ src/MsiServiceProvider.php
@@ -3,10 +3,8 @@
 namespace Rapidez\Msi;
 
 use Illuminate\Support\ServiceProvider;
-use Rapidez\Core\Models\Product;
 use Rapidez\Msi\Http\Middleware\DetermineAndSetStockId;
 use Rapidez\Msi\Models\Scopes\Product\WithProductStockScopeMsi;
-use Rapidez\Msi\Models\Scopes\Product\WithStockQtyScope;
 use TorMorten\Eventy\Facades\Eventy;
 
 class MsiServiceProvider extends ServiceProvider
@@ -20,31 +18,5 @@ class MsiServiceProvider extends ServiceProvider
     public function boot()
     {
         Eventy::addFilter('product.scopes', fn($scopes) => array_merge($scopes ?: [], [WithProductStockScopeMsi::class]));
-
-        Eventy::addFilter('product.children.select', function ($select) {
-            $stockId = config('rapidez.stock_id', (new WithProductStockScopeMsi)->getInventoryStockId());
-            // Replace the default "in_stock" with the MSI value.
-            return str_replace('"in_stock", children_stock.is_in_stock,'.PHP_EOL, '', $select).PHP_EOL.',"in_stock", (
-                SELECT is_salable
-                FROM inventory_stock_' . $stockId . '
-                WHERE inventory_stock_' . $stockId . '.sku = children.sku
-            )';
-        });
-
-        if (config('rapidez.frontend.expose_stock', config('rapidez.expose_stock'))) {
-            Eventy::addFilter('product.scopes', fn($scopes) => array_merge($scopes ?: [], [WithStockQtyScope::class]));
-
-            Eventy::addFilter('product.children.select', function ($select) {
-                $stockId = config('rapidez.stock_id', (new WithProductStockScopeMsi)->getInventoryStockId());
-                // Replace the default "qty" with the MSI value.
-                return str_replace('"qty", children_stock.qty,'.PHP_EOL, '', $select).PHP_EOL.',"qty", (
-                    SELECT ANY_VALUE(inventory_stock_' . $stockId . '.quantity) + COALESCE(SUM(inventory_reservation.quantity),0)
-                    FROM inventory_stock_' . $stockId . '
-                    LEFT JOIN inventory_reservation ON inventory_stock_' . $stockId . '.sku = inventory_reservation.sku
-                    WHERE inventory_stock_' . $stockId . '.sku = children.sku
-                    GROUP BY inventory_reservation.sku
-                )';
-            });
-        }
     }
 }
